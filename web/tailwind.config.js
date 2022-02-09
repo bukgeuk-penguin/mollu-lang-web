6436fa6bf2fdb78a882af270f5fd9c7332cdada8
Gangjun Lee <me@gangjun.dev>
2022-02-09 11:45:58 +0900

    initialize project

---
+++ web/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/layouts/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
