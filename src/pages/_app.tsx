999a5222652bcaf0af5ff6b4b16c753fc6edf6fe
Gangjun Lee <me@gangjun.dev>
2022-02-10 20:55:19 +0900

    setup umami analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEB_ID}
+          src={process.env.NEXT_PUBLIC_UMAMI_SRC}
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
